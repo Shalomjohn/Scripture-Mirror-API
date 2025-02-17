287653a4fc7231420af3f346e5a72bdc25a578db
Shalom John <shaliz.jhay@gmail.com>
2025-02-17 13:54:54 +0100

    🐛 Fix: fixed auth token validation

--- api/middlewares/authenticationMiddleware.js
+++ api/middlewares/authenticationMiddleware.js
@@ -4,32 +4,18 @@ const User = require('../models/user');
 
 const authenticate = async (req, res, next) => {
     try {
-        const authHeader = req.header('Authorization');
+        const bearerToken = req.header('Authorization');
 
-        const token = authHeader && authHeader.split(' ')[1];
-
+        const token = bearerToken.split(' ')[1];
 
         if (!token) {
             return res.status(401).json({ message: 'Unauthorized - Token not provided' });
         }
 
-        var decoded
-
-        jwt.verify(token, process.env.JWT_SECRET_KEY, (err, _decoded) => {
-            if (err) {
-                console.error('Token verification error:', err.name, err.message);
-                return res.status(403).json({ error: 'Invalid token' });
-            }
-
-            decoded = _decoded;
-
-            next();
-        });
-
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
         try {
-            console.log(`${decoded.user}`)
-            const user = await User.findById(decoded.user);
+            const user = await User.findById(decoded.id);
 
             if (!user) {
                 return res.status(401).json({ message: 'Unauthorized - Invalid token' });
