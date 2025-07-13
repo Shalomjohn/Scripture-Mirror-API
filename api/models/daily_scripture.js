04cd5d2a4128578f9e8862396faef2d95c19091d
Shalom John <shaliz.jhay@gmail.com>
2025-07-13 19:00:24 +0100

    🚑️ Made dailyscripture date non-unique

--- api/models/daily_scripture.js
+++ api/models/daily_scripture.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const DailyScriptureSchema = new mongoose.Schema({
-    date: { type: Date, default: Date.now, unique: true }, // One scripture per day
+    date: { type: Date, default: Date.now }, // One scripture per day
     verse: { type: String, required: true },  // e.g., "John 3:16"
     text: { type: String, required: true }    // Full scripture text
 });
