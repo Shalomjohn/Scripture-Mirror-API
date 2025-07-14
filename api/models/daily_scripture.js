636194b01475d320ddec4c3917ad9403b5c90d86
Shalom John <shaliz.jhay@gmail.com>
2025-07-14 13:43:03 +0100

    🐛 Made daily scripture data uniqueness false

--- api/models/daily_scripture.js
+++ api/models/daily_scripture.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const DailyScriptureSchema = new mongoose.Schema({
-    date: { type: Date, default: Date.now }, // One scripture per day
+    date: { type: Date, default: Date.now, unique: false }, // One scripture per day
     verse: { type: String, required: true },  // e.g., "John 3:16"
     text: { type: String, required: true },    // Full scripture text
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
