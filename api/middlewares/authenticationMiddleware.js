ac8efa08ddae8288018d05ee6e59bc12c4c12d06
Shalom John <shaliz.jhay@gmail.com>
2025-09-06 13:53:40 +0100

    📝 Now giving better no-auth errors

--- api/middlewares/authenticationMiddleware.js
+++ api/middlewares/authenticationMiddleware.js
@@ -6,10 +6,14 @@ const authenticate = async (req, res, next) => {
     try {
         const bearerToken = req.header('Authorization');
 
+        if (!bearerToken) {
+            return res.status(401).json({ message: 'Unauthorized - No token provided' });
+        }
+
         const token = bearerToken.split(' ')[1];
 
         if (!token) {
-            return res.status(401).json({ message: 'Unauthorized - Token not provided' });
+            return res.status(401).json({ message: 'Unauthorized - Token not properly provided' });
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
